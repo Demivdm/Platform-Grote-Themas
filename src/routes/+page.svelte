ab5ac93c69e4720ed36861431dcc1832d6c02bca
Demivdm <demi.van.der.maarl@hva.nl>
2023-10-12 09:39:40 +0200

    added border colors

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -107,7 +107,9 @@
 				<a href="/{werkvorm.link}"><svg width="50" height="53" viewBox="0 0 23 16" fill="none" xmlns="http://www.w3.org/2000/svg">
 					<path d="M22.7071 8.70711C23.0976 8.31658 23.0976 7.68342 22.7071 7.29289L16.3431 0.928932C15.9526 0.538408 15.3195 0.538408 14.9289 0.928932C14.5384 1.31946 14.5384 1.95262 14.9289 2.34315L20.5858 8L14.9289 13.6569C14.5384 14.0474 14.5384 14.6805 14.9289 15.0711C15.3195 15.4616 15.9526 15.4616 16.3431 15.0711L22.7071 8.70711ZM0 9H22V7H0L0 9Z" fill="white"/>
 					</svg>Bekijk video</a>
+					{#each werkvorm.tags as tag}
 					<p class="tag">{werkvorm.tag}</p>
+					{/each}
 			</div>
 			
 			</article>
@@ -198,27 +200,27 @@ margin: 3em;
 	}
 
 	.focus li:first-of-type{
-		border: 1px solid lightblue;
+		border: 4px solid #66E5BF;
 	}
 
 	.doel{
 		grid-area: 1 / 2 / 2 / 3;
 	}
 	.doel li:first-of-type{
-		border: 1px solid lightblue;
+		border: 4px solid var(--color-hva-pink);
 	}
 
 	.onderwerp{
 		grid-area: 1 / 3 / 2 / 4;
 	}
 	.onderwerp li:first-of-type{
-		border: 1px solid lightblue;
+		border: 4px solid #AEA4E5;
 	}
 	.fase{
 		grid-area: 1 / 4 / 2 / 5;
 	}
 	.fase li:first-of-type{
-		border: 1px solid lightblue;
+		border: 4px solid #FEF84C;
 	}
 
 	.input-search{
